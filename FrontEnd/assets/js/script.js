d8ba9c0e91e530be5b415092954941526573af96
Bruno <alvalade@live.fr>
2026-01-31 20:17:35 +0100

    intégration modale vue ajout photo et du formulaire. Chargement des catégories dynamiquement depuis l'API dans le select

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -2,11 +2,14 @@
 // Données*/
 //****************************************************** */
 
+
 //*********************************************************
 // État*/
 //****************************************************** */
 
 let listeWorks = [];
+
+let listeCategories = [];
 
 const token = localStorage.getItem("token");
 
@@ -29,8 +32,16 @@ const logInOut = document.getElementById("login-out");
 const bandeauNoir = document.querySelector(".bandeau-noir");
 const blocFIltres = document.querySelector(".bloc-filtres");
 const btnModifierProjets = document.querySelector(".modifier-projets");
-const modaleGalerie = document.getElementById("modale-gallerie")
-
+const modaleGalerie = document.getElementById("modale-gallerie");
+const listeImagesModale = document.querySelector(".images-modales");
+const iconeCroixFermeture = document.querySelector(".fermeture-modale");
+const btnAjouterPhoto = document.querySelector(".btn-ajouter-img");
+const btnRetourGalerie = document.querySelector(".retour");
+const vueListe = document.querySelector(".vue-liste");
+const vueAjout = document.querySelector(".vue-ajout");
+const btnValiderFormulaire = document.querySelector(".btn-valider");
+const titreModale = document.querySelector("#titre-modale");
+const select = document.getElementById("categorie-photo");
 
 //*********************************************************
 // API */
@@ -47,6 +58,16 @@ async function chargerWorksDepuisApi() {
         listeWorks = await reponseApiWorks.json();
     }
 
+}
+
+async function chargerCategoriesForm () {
+    const reponseApiCategories = await fetch (`${apiUrl}/categories`);
+
+    if (!reponseApiCategories.ok) {
+        console.error("Erreur chargement API:", reponseApiCategories.status, reponseApiCategories.statusText);
+    } else {
+        listeCategories = await reponseApiCategories.json ();
+    }
 }
 
 
@@ -130,9 +151,126 @@ function filtrerWorksParCategorie(boutonClique) {
     return worksAAfficher;
 }
 
+function remplirModaleImagesWorks() {
+    listeImagesModale.innerHTML = "";
+
+
+    for (let i = 0; i < listeWorks.length; i++) {
+
+        const work = listeWorks[i];
+
+        const figure = document.createElement("figure");
+        const imageModale = document.createElement("img");
+        imageModale.src = work.imageUrl;
+        const btnCorbeille = document.createElement("button");
+        btnCorbeille.classList.add("btn-corbeille");
+        btnCorbeille.setAttribute("aria-label", "supprimer")
+        btnCorbeille.dataset.id = work.id;
+        const logoCorbeille = document.createElement("i");
+        logoCorbeille.classList.add("fa-solid", "fa-trash-can");
+
+        figure.appendChild(imageModale);
+        figure.appendChild(btnCorbeille);
+        listeImagesModale.appendChild(figure);
+        btnCorbeille.appendChild(logoCorbeille);
+
+    }
+}
+
+function auClicModifier(event) {
+
+    remplirModaleImagesWorks();
+    ecouterClicBtnCorbeille();
+    modaleGalerie.showModal();
+}
+
+function fermerModaleX(event) {
+    modaleGalerie.close();
+
+}
+
+function fermerModale(event) {
+    if (event.target === event.currentTarget) {
+        modaleGalerie.close();
+    }
+}
+
+async function supprimerWork(idWork) {
+    const reponseApiWorks = await fetch(`${apiUrl}/works/${idWork}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (!reponseApiWorks.ok) {
+        console.error("Erreur chargement :", reponseApiWorks.status);
+        return;
+    }
+
+    await chargerWorksDepuisApi();
+
+    remplirModaleImagesWorks();
+
+    const divGallery = recupererElement(".gallery");
+    afficherWorks(divGallery, listeWorks);
+}
+
+function remplirFormCategories () {
+    for (let i = 0; i < listeCategories.length; i ++) {
+
+        const categorie = listeCategories[i];
+
+        const option = document.createElement ("option");
+        option.value = categorie.id;
+        option.text = categorie.name;
+
+        select.appendChild(option);
+
+    }
+}
+
+async function nomATrouver () {
+    await chargerCategoriesForm ();
+    remplirFormCategories();
+
+}
+
+
+
 //*********************************************************
 // Événements*/
 //****************************************************** */
+function ecouterClicBtnCorbeille() {
+
+    listeImagesModale.addEventListener("click", function (event) {
+        const btnSupprimer = event.target.closest(".btn-corbeille");
+        if (!btnSupprimer) return;
+        const idWork = btnSupprimer.dataset.id;
+        supprimerWork(idWork);
+    });
+}
+
+function basculerVueAjout(event) {
+    vueListe.classList.add ("oculter");
+    btnRetourGalerie.classList.remove ("oculter");
+    btnAjouterPhoto.classList.add ("oculter");
+    btnValiderFormulaire.classList.remove ("oculter");
+    titreModale.innerText = "Ajout photo";
+    vueAjout.classList.remove ("oculter");
+
+}
+
+function basculerVueGalerie (event) {
+    vueAjout.classList.add ("oculter");
+    vueListe.classList.remove("oculter");
+    btnRetourGalerie.classList.add("oculter");
+    btnAjouterPhoto.classList.remove("oculter");
+    btnValiderFormulaire.classList.add("oculter");
+    titreModale.innerText = "Galerie photo";
+}
+
+
 
 //*********************************************************
 // Initialisation*/
@@ -151,7 +289,7 @@ async function initialisation() {
         afficherWorks(divGallery, worksAAfficher);
     }
     );
-    
+
     console.log("token page accueil =", localStorage.getItem("token"));
 }
 
@@ -166,19 +304,27 @@ if (token !== null && token !== undefined) {
     blocFIltres.remove();
     btnModifierProjets.classList.remove("oculter");
 
-
-
-    logInOut.addEventListener("click", function (event){
+    logInOut.addEventListener("click", function (event) {
         event.preventDefault();
-        localStorage.removeItem ("token");
+        localStorage.removeItem("token");
         window.location.reload();
     });
 
-} 
+    btnModifierProjets.addEventListener("click", auClicModifier);
+    iconeCroixFermeture.addEventListener("click", fermerModale);
+    modaleGalerie.addEventListener("click", fermerModale);
+    btnAjouterPhoto.addEventListener("click", basculerVueAjout);
+    btnRetourGalerie.addEventListener("click", basculerVueGalerie);
 
-btnModifierProjets.addEventListener("click", () => {
-    modaleGalerie.showModal();
-});
+    nomATrouver();
+
+}
+
+
+
+
+
+
 
 
 
